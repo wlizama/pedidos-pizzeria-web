8a5885531caae779ed3d85a3108aa5bbcb28dae3
Wilder H. Lizama Garcia <wilderlizama@gmail.com>
2021-09-10 17:28:40 -0500

    Primeras pruebas jsf primefaces

---
+++ src/main/java/com/ci2/ppw/controller/NewJSFManagedBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ci2.ppw.controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author wilderlizama
+ */
+@Named(value = "newJSFManagedBean")
+@RequestScoped
+public class NewJSFManagedBean {
+
+    /**
+     * Creates a new instance of NewJSFManagedBean
+     */
+    public NewJSFManagedBean() {
+    }
+    
+}
