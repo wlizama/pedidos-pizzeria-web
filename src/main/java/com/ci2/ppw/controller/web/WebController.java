790f45f40c824ac749b6661adc56da0f1bcbe41a
Wilder H. Lizama Garcia <wilderlizama@gmail.com>
2021-10-23 17:50:13 -0500

    Controlador incial intranet

--- src/main/java/com/ci2/ppw/controller/web/WebController.java
+++ src/main/java/com/ci2/ppw/controller/web/WebController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-public class InicioController {
+public class WebController {
 
     @RequestMapping
     public String inicio() {
