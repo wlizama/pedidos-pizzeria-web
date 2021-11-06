2c9ec00071747a62099b4d65d95314fc77100d15
Wilder H. Lizama Garcia <wilderlizama@gmail.com>
2021-11-06 18:09:21 -0500

    1er hibernate test

--- src/main/java/com/ci2/ppw/config/AppInitializer.java
+++ src/main/java/com/ci2/ppw/config/AppInitializer.java
@@ -5,23 +5,22 @@
  */
 package com.ci2.ppw.config;
 
-//import javax.servlet.FilterRegistration;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class AppInitializer implements WebApplicationInitializer {
-    
-    @Override
-    public void onStartup( ServletContext servletContext  ) throws ServletException {
-        AnnotationConfigWebApplicationContext webCtx = new AnnotationConfigWebApplicationContext();
-        webCtx.register(WebMvcConfig.class);
-        webCtx.setServletContext(servletContext);
-        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(webCtx));
-        servlet.setLoadOnStartup(1);
-        servlet.addMapping("/");
-    }
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+   @Override
+   protected Class<?>[] getRootConfigClasses() {
+      return new Class[] { HibernateConfig.class/*, SecurityConfig.class*/ };
+   }
+
+   @Override
+   protected Class<?>[] getServletConfigClasses() {
+      return new Class[] { WebMvcConfig.class };
+   }
+
+   @Override
+   protected String[] getServletMappings() {
+      return new String[] { "/" };
+   }
 }
--- src/main/java/com/ci2/ppw/config/WebMvcConfig.java
+++ src/main/java/com/ci2/ppw/config/WebMvcConfig.java
@@ -5,10 +5,13 @@
  */
 package com.ci2.ppw.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,9 +20,9 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = { "com.ci2.ppw.controller" })
+@ComponentScan(basePackages = { "com.ci2.ppw" })
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Bean
+   @Bean
     public InternalResourceViewResolver resolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setViewClass(JstlView.class);
@@ -27,11 +30,25 @@ public class WebMvcConfig implements WebMvcConfigurer {
         resolver.setSuffix(".jsp");
         return resolver;
     }
-
+    
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
           .addResourceHandler("/resources/**")
           .addResourceLocations("/resources/");	
     }
+
+//    @Bean
+//    public MessageSource messageSource() {
+//        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+//        source.setBasename("messages");
+//        return source;
+//    }
+
+//    @Override
+//    public Validator getValidator() {
+//        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+//        validator.setValidationMessageSource(messageSource());
+//        return validator;
+//    }
 }
