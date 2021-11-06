c16dc9400b577be98e08c0390a7dbc7d7d5a3e7b
Jose Garcia <jgarciaguti0@gmail.com>
2021-11-06 11:27:27 -0500

    HolaLogin

---
+++ src/main/java/com/ci2/ppw/model/Validar.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ci2.ppw.model;
+
+/**
+ *
+ * @author JoseGaGu
+ */
+public interface Validar {
+    public int validar(Usuario usr);
+    
+}
